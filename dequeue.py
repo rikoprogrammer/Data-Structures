493767c8cc08688d07c0868b5b8f82c9902d3752
Eric <erikodongo.35@gmail.com>
2020-10-03 17:35:07 +0300

    add queue and dqqueue sections

---
+++ dequeue.py
@@ -0,0 +1,39 @@
+"""
+Dequeue - this is a collection of items where items can be added from either side, does not conform,
+to LIFO nor FIFO rules.
+
+"""
+
+#Implementation
+
+class dequeue(object):
+	def __init__(self):
+		self.items = []
+
+	def isEmpty(self):
+		return self.items == []
+
+	def addFront(self, item):
+		self.items.append(item)
+
+	def addRear(self, item):
+		self.items.insert(0, item)
+
+	def removeFront(self, item):
+		return self.items.pop()
+
+	def removeRear(self, item):
+		return self.items.pop(0)
+
+	def size(self):
+		return len(self.items)
+
+#Create an an instance and test some methods
+
+d = dequeue()
+d.addFront(1)
+d.addRear(2)
+d.addFront('car')
+d.removeRear(0)
+print(d.size())
+
---
+++ queus.py
@@ -0,0 +1,36 @@
+""" Queues - just like normal queues, this work on a FIFO basis, i.e first in first out
+or first come first serve.
+
+"""
+
+#Implementation
+
+class queue(object):
+
+	def __init__(self):
+		self.items = []
+
+	def isEmpty(self):
+		return self.items == []
+
+	def enqueu(self, item):
+		self.items.insert(0, item)
+
+	def dequeue(self):
+		return self.items.pop()
+
+	def size(self):
+		return len(self.items)
+
+#Create an instance of the class and test some of its methods
+
+q = queue()
+q.enqueu(22)
+q.enqueu(33)
+q.enqueu('bus')
+q.enqueu('boy')
+q.dequeue()
+print(q.isEmpty())
+print(q.size())
+
+
